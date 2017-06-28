fa491478e3af3c319981c96a7a5546efdd176ab1
RedDucks <jonbarrow1998@gmail.com>
2017-06-28 04:15:37 -0400

    Initial files

---
+++ test/script.js
@@ -0,0 +1,23 @@
+var options = [
+				{
+					icon:'1',
+					label: 'Run alert function Run alert functionRun alert functionRun alert function',
+					callback: function() {
+						alert('dasd');
+					}
+				},
+				{
+					icon:'<i class="fa fa-id-card" aria-hidden="true"></i>',
+					label: 'Test Element 2'
+				},
+				{
+					icon:'3',
+					label: 'Test Element 3',
+					display: {
+						color: 'blue',
+						background: 'red'
+					}
+				}
+			];
+
+var context = new Bubbler(options);
